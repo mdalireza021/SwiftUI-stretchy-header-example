24ec85c2621619135528e2fafba0f0e75f70769c
md ali reza <muhammd.ali@brainstation-23.com>
2023-07-03 14:13:46 +0600

    Initial Commit

---
+++ SwiftUI Stretchy Header/SwiftUI_Stretchy_HeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Stretchy_HeaderApp.swift
+//  SwiftUI Stretchy Header
+//
+//  Created by BS1096 on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Stretchy_HeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
